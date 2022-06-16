21f7b4308d90b3b645fa8ad9737327aa250ca42b
maverickkhofanda <maverickkhofanda@gmail.com>
2022-06-16 10:28:23 +0700

    commit aja

--- hello.js
+++ hello.js
@@ -6,3 +6,35 @@ arr = [
     1,
     function() {console.log("hi!");}
 ];
+arr[1]();
+
+
+////////////////
+
+const obj = {};
+obj.firstName = "lele";
+obj.lastName = "kelelep";
+obj.greet = function() {
+    console.log("blub blub blub");
+};
+
+///////////////////
+const o = {
+    a: 'a',
+    b: 'b',
+    obj: {
+        key: "key"
+    }
+};
+const o2 = o;
+const o3 = Object.assign({}, o);
+
+o2.obj.key = 'NEW VALUE';
+o.a = "new value";
+
+console.log(o2.a);
+console.log(o3.a);
+
+console.log(o.obj.key);
+console.log(o2.obj.key);
+console.log(o3.obj.key);
